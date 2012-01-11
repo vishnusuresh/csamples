0605183e0d9bec09b206896f65757dd36aa53c3c
Vishnu Suresh <Vishnu.Suresh@toumaz.com>
2012-01-11 16:26:28 +0000

    Pointer Dereferecing comments
    Signed-off-by: Vishnu Suresh <Vishnu.Suresh@toumaz.com>

--- 036_dereferece_pointer.c
+++ 036_dereferece_pointer.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	int i = 10;
 	int *pi;
 
-	printf("Contents of ptr is %02X\n", *pi);
+	printf("Contents of Random ptr is %02X\n", *pi);
 	printf("I is %d and ptr to i is %02X\n", i, pi);
 	pi = &i;
 	printf("Contents of ptr to i is %02X\n", *pi);
